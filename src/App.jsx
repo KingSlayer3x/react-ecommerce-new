9654036eabdb1b853b0d68185dd80cc0a2a6efd1
KingSlayer3x <alito321a@gmail.com>
2026-07-10 16:00:55 +0300

    some examples..

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-
+import { useState, useEffect } from 'react';
 import './App.css'
-// import SignUpForm from '../components/SignUpForm';
-import { Route, Routes } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import HomePage from '../components/HomePage';
-import ProfilePage from '../components/ProfilePage';
-import LoginPage from '../components/LoginPage';
-import { AuthContext } from './assets/AuthContext';
-import { useState } from 'react';
+
 function App() {
-    const [user, setUser] = useState({name: "", isAuth: false});
-    function login(name){
-      setUser({name: name, isAuth: true})
-    }
-    function logout(){
-      setUser({name: "", isAuth: false});
-    }
+    const [showCounter, setShowCounter] = useState(false);
+    // mount > update > unmount
   return (
     <div>
-      {/* <SignUpForm /> */}
-      <AuthContext.Provider value={{ user,login, logout }}>
-      <Navbar />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/profile' element={<ProfilePage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='*' element={<h1 style={{padding: "1rem"}}>404 Not Found</h1>}/>
-        </Routes>
-      </AuthContext.Provider>
+      <button onClick={() => setShowCounter(!showCounter)}>{" "}Show Counter</button>
+      {showCounter && (
+        <Counter />
+        )}
     </div>
   )
 }
 
 export default App;
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+      console.log("Component Mounted");
+      return () => {
+        console.log("Component Unmounted");
+      }
+    },[])
+    useEffect(()=> {
+      console.log("component updated");
+    },[count]);
+  return(
+    <button onClick={() => setCount(count + 1)}>{count}</button>
+  )
+}
