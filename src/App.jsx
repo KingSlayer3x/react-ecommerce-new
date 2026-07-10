ff3d4afde7864da6e0e91c7607a6ccac4ccc10d4
KingSlayer3x <alito321a@gmail.com>
2026-07-10 22:06:59 +0300

    added auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import Home from './components/Home';
 import Auth from './components/Auth';
 import Checkout from './components/Checkout';
 import Navbar from './components/Navbar';
+import AuthProvider from './context/AuthContext';
+
 function App() {
  return (
-  <div className='app'>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/auth' element={<Auth />}/>
-      <Route path='/checkout' element={<Checkout />}/>
-    </Routes>
-  </div>
+  <AuthProvider>
+    <div className='app'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/auth' element={<Auth />}/>
+        <Route path='/checkout' element={<Checkout />}/>
+      </Routes>
+    </div>
+  </AuthProvider>
  );
 }
 export default App;
