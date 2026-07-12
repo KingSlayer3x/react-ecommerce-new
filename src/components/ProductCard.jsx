c6e6f18013cc7201f7582e3ee67f885660e9537f
KingSlayer3x <alito321a@gmail.com>
2026-07-12 22:44:56 +0300

    added product details and view details

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,17 +2,17 @@ import { Link } from "react-router-dom"
 export default function ProductCard({product}) {
   return (
     <div>
-        <div className="product-card">
-            <img src={product.image} className="product-card-image" alt="product-image" />
-            <div className="product-card-content">
-                <h3 className="product-card-name">{product.name}</h3>
-                <p className="product-card-price">{product.price}</p>
-                <div className="product-card-action">
-                    <Link className="btn btn-secondary">View Details</Link>
-                    <button className="btn btn-primary">Add to Cart</button>
-                </div>
-            </div>
-        </div>
+     <div className="product-card">
+      <img src={product.image} className="product-card-image" alt="product-image" />
+      <div className="product-card-content">
+       <h3 className="product-card-name">{product.name}</h3>
+       <p className="product-card-price">{product.price}</p>
+       <div className="product-card-action">
+        <Link className="btn btn-secondary" to={`/products/${product.id}`}>View Details</Link>
+        <button className="btn btn-primary">Add to Cart</button>
+       </div>
+      </div>
+     </div>
     </div>
   )
 }
